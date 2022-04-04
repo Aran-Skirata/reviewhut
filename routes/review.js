74aa799eb674a9be7e67e7d3df70d53cd58bbe91
Karol Płonka <plonkakarol1@gmail.com>
2022-04-04 13:30:56 +0200

    refactoring

--- routes/review.js
+++ routes/review.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const asyncErrorHandler = require("../utils/AsyncErrorHandler");
-const Review = require("../models/review");
-const Place = require("../models/place");
 const router = express.Router({ mergeParams: true });
 const {
   isLoggedIn,
