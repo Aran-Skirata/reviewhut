7c5991b4adfe341a80f12a5072a7ff340341e778
Karol Płonka <plonkakarol1@gmail.com>
2022-04-15 11:44:25 +0200

    XSS and CSP

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Place = require("./models/place");
-const Review = require("./models/review");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
 const asyncErrorHandler = require("./utils/AsyncErrorHandler");
-const placeValidateSchema = require("./validations/placeValidations");
-const reviewValidateSchema = require("./validations/reviewValidations");
 const session = require("express-session");
 const flash = require("connect-flash");
 const reviewRoutes = require('./routes/review');
@@ -21,6 +18,8 @@ const userRoutes = require('./routes/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
 
 mongoose
     .connect(process.env.MONGO_URI)
@@ -40,11 +39,13 @@ app.use(methodOverride("_method"));
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname,'public')));
 app.use(session({
+    name: "_s",
     secret: "thisisnotagoodsecret",
     resave: false,
     saveUninitialized: true,
     cookie:{
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
@@ -52,6 +53,18 @@ app.use(session({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(mongoSanitize());
+// app.use(helmet());
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       default: [],
+//       scriptSrc: ["'unsafe-inline'","'self'","https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"],
+//       styleSrc: ["'self'","'unsafe-inline'","https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"],
+//       imgSrc : ["'self'","blob:","data:","https://res.cloudinary.com/dr8m8qurw","https://picsum.photos"]
+//     },
+//   })
+// );
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -70,6 +83,7 @@ app.use('/',userRoutes);
 
 
 app.get("/", asyncErrorHandler(async (req, res) => {
+    console.log(req.query);
     const places = (await Place.find({}).limit(3));
     res.render("home", {places});
 }));
